b9c8bb87152a15e988f99148983b8e06c1cee9c5
tomyc <tomyc@users.noreply.github.com>
2017-09-18 20:53:07 +0200

    Update python kursy walut xml.sql

--- python kursy walut xml.sql
+++ python kursy walut xml.sql
@@ -1,3 +1,19 @@
+/* Copyright (c) 2017 Agnieszka Cieplak
+ *
+ * It is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with CERMINE. If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
 CREATE TABLE #Waluty(waluta char(4),kupno nvarchar(max), sprzedaz nvarchar(max))
 
 insert into #Waluty exec 
